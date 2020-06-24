d7bc32db657ba113f25a4feda7741b0f02da9a66
Tobias Ruano <ruano.t10@gmail.com>
2020-06-24 17:19:12 -0300

    follower model created

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Tobias Ruano on 24/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
