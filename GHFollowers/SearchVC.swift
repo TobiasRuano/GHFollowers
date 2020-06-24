15b489296abe48974ad2c71aafd0b4196e0d86dc
Tobias Ruano <ruano.t10@gmail.com>
2020-06-24 12:14:12 -0300

    created tabbar and navigation controller basic logic. Also created searchVC and favoritesVC

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Tobias Ruano on 24/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
