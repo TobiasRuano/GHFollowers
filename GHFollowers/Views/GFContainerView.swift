f05467633c52307a9e4ab5c7a55853e50bb6a11e
Tobias Ruano <ruano.t10@gmail.com>
2020-06-24 17:03:51 -0300

    Refactored conteinerView into its own sub class

---
+++ GHFollowers/Views/GFContainerView.swift
@@ -0,0 +1,34 @@
+//
+//  GFContainerView.swift
+//  GHFollowers
+//
+//  Created by Tobias Ruano on 24/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+class GFContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
