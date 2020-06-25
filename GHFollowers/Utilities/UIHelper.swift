3a80f613cf07d49a4af00816bfc47d42d1136844
Tobias Ruano <ruano.t10@gmail.com>
2020-06-25 13:55:45 -0300

    Refactored createThreeColumnFlowLayout function

---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Tobias Ruano on 25/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let witdh = view.bounds.width
+        let padding: CGFloat = 12
+        let minimunItemSpacing: CGFloat = 10
+        let availableWidth = witdh - (padding * 2) - (minimunItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
