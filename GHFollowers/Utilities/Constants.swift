ef12bee01615ad0519e15d16517ce3490c937b7c
Tobias Ruano <ruano.t10@gmail.com>
2020-06-26 14:18:30 -0300

    First part of the userInfoVC implemented

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Tobias Ruano on 26/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
---
+++ GHFollowers/Views/Labels/GFSecondaryTitleLable.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLable.swift
+//  GHFollowers
+//
+//  Created by Tobias Ruano on 26/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLable: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
